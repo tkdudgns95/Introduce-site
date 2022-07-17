0e931328c4947a5a9fec71163282dd5e50f4a862
tkdudgns95 <tkdudgns95@naver.com>
2022-07-17 16:39:58 +0900

    JS 이미지 슬라이더

--- portfolio.js
+++ portfolio.js
@@ -28,3 +28,54 @@ function menuToggle() {
 }
 
 document.getElementById('toggleBtn').addEventListener('click', menuToggle);
+
+/* WELCOME AREA */
+var imageSlideIndex = 1;
+
+showImageSlides(imageSlideIndex);
+
+function imageSlideTimer() {
+    plusImageSlides(1);
+}
+
+var imageTimer = setInterval(imageSlideTimer, 3000);
+
+function plusImageSlides(n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 3000);
+
+    showImageSlides(imageSlideIndex += n);
+}
+
+function currentImageSlide(n) {
+    clearInterval(imageTimer);
+    imageTimer = setInterval(imageSlideTimer, 3000);
+    
+    showImageSlides(imageSlideIndex = n);
+
+}
+
+function showImageSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName('image-slide');
+    var dots = document.getElementsByClassName('dot');
+    if(n > slides.length) { imageSlideIndex = 1}
+    if(n < 1) { imageSlideIndex = slides.length }
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[imageSlideIndex - 1].style.display = 'block';
+    dots[imageSlideIndex - 1].className += ' active';
+
+}
+
+document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null, -1));
+document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null, 1));
+
+document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null, 1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null, 2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null, 3));
+document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null, 4));
