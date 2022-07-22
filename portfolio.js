e61cfd8e0f1045e700f866bdcd2caf8a1f3cf971
tkdudgns95 <tkdudgns95@naver.com>
2022-07-22 12:21:41 +0900

    REVIEW JS

--- portfolio.js
+++ portfolio.js
@@ -156,3 +156,50 @@ var filterItems = document.getElementsByClassName('overlay');
 for(var i=0;i<filterItems.length;i++) {
     filterItems[i].addEventListener('click', viewPortfolio);
 }
+
+/* REIVEW AREA */
+var reviewSlideIndex = 0;
+
+function reviewSlideTimer() {
+    plusReviewSlides(1);
+
+}
+
+var reviewTimer = setInterval(reviewSlideTimer, 5000); 
+
+
+
+function plusReviewSlides(n) {
+    clearInterval(reviewTimer);
+    reviewTimer = setInterval(reviewSlideTimer, 5000);
+    showReviewSlides(reviewSlideIndex += n);
+}
+
+function showReviewSlides(n) {
+    var i;
+    var review_slides = document.getElementsByClassName('review-slide');
+
+    if(n > review_slides.length - 3) {
+        reviewSlideIndex = 0;
+    }
+
+    if(n < 0) {
+        reviewSlideIndex = review_slides.length - 3;
+    }
+
+    for(i = 0; i < review_slides.length; i++) {
+        removeClass(review_slides[i], 'show');
+        removeClass(review_slides[i], 'res-show');
+        addclass(review_slides[i], 'hide');
+    }
+
+    removeClass(review_slides[reviewSlideIndex], 'hide');
+    addclass(review_slides[reviewSlideIndex], 'res-show');
+    removeClass(review_slides[reviewSlideIndex+1], 'hide');
+    addclass(review_slides[reviewSlideIndex+1], 'show');
+    removeClass(review_slides[reviewSlideIndex+2], 'hide');
+    addclass(review_slides[reviewSlideIndex+2], 'show');
+}
+
+document.getElementById('reviewPrev').addEventListener('click', plusReviewSlides.bind(null, -1));
+document.getElementById('reviewNext').addEventListener('click', plusReviewSlides.bind(null, 1));
